25fb60de1fbc7ce60cb49be02cdaf7be04a0b6f4
TimeChaser <1872101334@qq.com>
2023-06-16 15:24:53 +0800

    docs: modify artifact id

--- src/test/java/top/isopen/commons/springboot/bean/OrderByRequestTest.java
+++ src/test/java/top/isopen/commons/springboot/bean/OrderByRequestTest.java
@@ -1,6 +1,8 @@
 package top.isopen.commons.springboot.bean;
 
 import junit.framework.TestCase;
+import top.isopen.commons.logging.Log;
+import top.isopen.commons.logging.LogFactory;
 import top.isopen.commons.springboot.types.OrderBy;
 
 import java.util.ArrayList;
@@ -9,10 +11,13 @@ import java.util.stream.Collectors;
 
 public class OrderByRequestTest extends TestCase {
 
-    void testTransformToType() {
+    private static final Log log = LogFactory.getLog(OrderByRequestTest.class);
+
+    public void testTransformToType() {
         List<OrderByRequest> orderByRequestList = new ArrayList<>();
         orderByRequestList.add(new OrderByRequest("cole", true));
         List<OrderBy> collect = orderByRequestList.stream().map(orderByRequest -> OrderBy.builder().build()).collect(Collectors.toList());
+        log.info(collect.toString());
     }
 
 }
